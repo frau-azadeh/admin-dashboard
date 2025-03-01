761f1d6cfda1666c022f620bff52ef9977c6821f
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-01 05:29:58 -0800

    add toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import { Header } from "./components/Header";
 
 function App() {
   return (
     <div>
+      <ToastContainer position="top-right" autoClose={3000} />
       <Header />
     </div>
   );
