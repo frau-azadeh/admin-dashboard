8b61fec87238679d2a4efff06fbbd8af85f325dc
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-01 05:20:20 -0800

    update prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import "./App.css";
 import { Header } from "./components/Header";
 
 function App() {
-
   return (
-
-      <div>
-        <Header />
+    <div>
+      <Header />
     </div>
   );
 }
