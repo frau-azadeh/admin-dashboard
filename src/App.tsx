34ef2190b8f0c4dd7656188d48ffe3327feb6c00
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-02 00:23:32 -0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "./store/store";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Setting from "./pages/Setting"; // مسیر اصلاح شد
+import ProtectedRoute from "./routes/ProtectedRoute";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import { Header } from "./components/Header";
 
 function App() {
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+
   return (
-    <div>
+    <Router>
       <ToastContainer position="top-right" autoClose={3000} />
-      <Header />
-    </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/setting" element={<Setting />} />
+        </Route>
+        {/* اگر مسیر اشتباه بود، به داشبورد یا لاگین هدایت کن */}
+        <Route path="*" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
