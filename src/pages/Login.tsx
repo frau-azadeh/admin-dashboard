596343335824295660341be97b0aa48baefd0b50
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-02 00:24:29 -0800

    add page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../store/slices/authSlice";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      toast.warning("⚠️ لطفاً همه فیلدها را پر کنید!");
+      return;
+    }
+
+    // شبیه‌سازی درخواست ورود به سرور
+    const fakeToken = "123456789abcdef";
+    dispatch(login({ user: email, token: fakeToken }));
+    toast.success("✅ ورود موفقیت‌آمیز بود!");
+    
+    setTimeout(() => {
+      navigate("/dashboard");
+    }, 1000);
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold text-center text-gray-700">ورود به پنل مدیریت</h2>
+        <form onSubmit={handleLogin} className="mt-4">
+          <div className="mb-4">
+            <label className="block text-gray-600">ایمیل</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-blue-300"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-600">رمز عبور</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-4 py-2 border rounded-lg focus:ring focus:ring-blue-300"
+            />
+          </div>
+          <button
+            type="submit"
+            className={`w-full py-2 rounded-lg transition text-white ${
+                email && password
+                ? "bg-blue-900 hover:bg-blue-600"
+                : "bg-gray-300 cursor-not-allowed"
+            }`}
+            disabled={!email || !password}
+            >
+  ورود
+        </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
