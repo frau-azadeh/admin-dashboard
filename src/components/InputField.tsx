a1b60d2a6db3f94399d10e9eba68cb45e9964f39
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-02 05:59:12 -0800

    add InputField component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+interface InputFieldProps{
+
+    label: string;
+    type: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>)=> void;
+}
+
+const InputField: React.FC<InputFieldProps> = ({label, type, value, onChange}) => {
+  return (
+    <div className='mb-4'>
+        <label className='block text-gray-600'>{label}</label>
+        <input
+            value={value}
+            type={type}
+            onChange={onChange}
+            className='w-full px-4 py-2 border rounded-lg focus:ring focus:ring-blue-300'
+        />
+    </div>
+  )
+}
+
+export default InputField
