3ebb7e07cce0324e9e26309dd00625c0131f6dd7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-03 01:51:16 -0800

    update prettier by login branches

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,36 +1,37 @@
-import React from 'react'
-import InputField from './InputField';
-import Button from './Button';
+import React from "react";
+import InputField from "./InputField";
+import Button from "./Button";
 interface LoginFromProps {
-
   email: string;
   password: string;
   setEmail: (email: string) => void;
   setPassword: (password: string) => void;
   onSubmit: (e: React.FormEvent) => void;
-
 }
-const LoginForm: React.FC<LoginFromProps> = ({email, password, setEmail, setPassword, onSubmit}) => {
+const LoginForm: React.FC<LoginFromProps> = ({
+  email,
+  password,
+  setEmail,
+  setPassword,
+  onSubmit,
+}) => {
   return (
-    <form onSubmit={onSubmit} role='form' className='mt-4'>
+    <form onSubmit={onSubmit} role="form" className="mt-4">
       <InputField
-        label='ایمیل'
-        type='email'
+        label="ایمیل"
+        type="email"
         value={email}
-        onChange={(e)=> setEmail(e.target.value)}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <InputField
-        label='پسورد'
+        label="پسورد"
         type="password"
         value={password}
-        onChange={(e) =>setPassword(e.target.value)}
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <Button 
-        label='ورود'
-        disabled={!email || !password}
-      />
+      <Button label="ورود" disabled={!email || !password} />
     </form>
-  )
-}
+  );
+};
 
 export default LoginForm;
