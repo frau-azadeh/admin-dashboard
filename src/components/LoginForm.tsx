3c17b1cba78239705e1d69081569ac1f061f6208
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-03-03 01:50:13 -0800

    refactor LoginForm Component

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,39 +1,36 @@
-import React from "react";
-import InputField from "./InputField";
-import Button from "./Button";
+import React from 'react'
+import InputField from './InputField';
+import Button from './Button';
+interface LoginFromProps {
 
-interface LoginFormProps {
   email: string;
   password: string;
   setEmail: (email: string) => void;
   setPassword: (password: string) => void;
   onSubmit: (e: React.FormEvent) => void;
-}
 
-const LoginForm: React.FC<LoginFormProps> = ({
-  email,
-  password,
-  setEmail,
-  setPassword,
-  onSubmit,
-}) => {
+}
+const LoginForm: React.FC<LoginFromProps> = ({email, password, setEmail, setPassword, onSubmit}) => {
   return (
-    <form onSubmit={onSubmit} className="mt-4" role="form">
+    <form onSubmit={onSubmit} role='form' className='mt-4'>
       <InputField
-        label="ایمیل"
-        type="email"
+        label='ایمیل'
+        type='email'
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(e)=> setEmail(e.target.value)}
       />
       <InputField
-        label="پسورد"
+        label='پسورد'
         type="password"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={(e) =>setPassword(e.target.value)}
       />
-      <Button label="ورود" disabled={!email || !password} />
+      <Button 
+        label='ورود'
+        disabled={!email || !password}
+      />
     </form>
-  );
-};
+  )
+}
 
 export default LoginForm;
